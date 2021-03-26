221b48662489fce368888f5c33172412c30fb876
Panister Fatheroy <panisterfatheroy@gmail.com>
2021-03-26 19:20:47 +0000

    more pictures

--- client/src/home.js
+++ client/src/home.js
@@ -6,7 +6,8 @@ function Home() {
 
 
 
-    const images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg']
+    const images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg']
+    const names = ['occidentalizing','paracelsus','semifinal','thamyris','unimpatient','tenebrist','nonvalued','vulcanism','predeplete','aalii']
 
 
 
@@ -21,9 +22,9 @@ function Home() {
                 <div className="card" key={i}>
                     <img className="card-img-top" src={`/assets/images/products/${img}`} alt="product" />
                     <div className="card-body">
-                        <h5 className="card-title">Title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">More...</a>
+                        <h5 className="card-title">{names[i]}</h5>
+                        <p className="card-text">Some information about the product.</p>
+                        <a href="#" className="btn btn-primary">More</a>
                     </div>
                 </div>
                 ))}
